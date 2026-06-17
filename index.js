29ffcb92592fc05612bff19957696edb8af1efd0
Aman Mishra <amanmishra3946@gmail.com>
2026-06-18 01:59:25 +0530

    Feature:Added Movie Model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const env = require('dotenv');
 const mongoose = require('mongoose');
+const Movie = require('./model/movie.model');
 env.config();
 
 const app = express();
@@ -20,6 +21,20 @@ app.listen(process.env.PORT, async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
         console.log("Successfully connected to MongoDB");
+
+  /**     await Movie.create({
+              name:"Bacchan Pandey",
+              description:"Comedy ,masala Movie",
+              casts:["Akshay Kumar","Kriti Sanon","Jaqueline Fernandiz"],
+              director:"Farhad Samji",
+              trailerUrl:"http://BacchanPandey/trailers/1",
+              releasedDate:"18-03-2022",
+              releaseStatus:"RELEASED",
+             lightMan:["Pratham Mishra","Praveen Tiwari"],
+              Verdict:"Average",
+        }); 
+        */
+
     } catch (err) {
         console.log("Not able to connect with MongoDB");
         console.error(err);
