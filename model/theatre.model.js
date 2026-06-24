d182175de7a734ed3f51eb884a51f605ec53534a
Aman Mishra <amanmishra3946@gmail.com>
2026-06-24 22:28:21 +0530

    Feat: Added theatre schema

---
+++ model/theatre.model.js
@@ -0,0 +1,24 @@
+const mongoose  = required('mongoose');
+/**
+ * Defines the Schema of theatre resource to be stored int the DB
+ */
+
+const theatreSchema = new mongoose.Schema({
+      name: {
+         type:String,
+         required:true
+      },
+      description: String ,
+      city: {
+          type:String,
+          required:true
+      },
+      pincode: {
+        type:Number,
+        required:true
+      },
+      address: String
+},{timestamps: true});
+
+const Theatre = mongoose.model('Theatre',theatreSchema);
+module.exports = Theatre;
